28ec2a05b79de97d55e7d6f031b09f3c421ba753
Jose Jofre <jjofre@cuyanas.com.ar>
2018-11-27 15:43:49 -0300

    commit

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -4,8 +4,12 @@ import { CLIENTES } from '../clientes/clietnes.json';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of, throwError} from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+
 
 
 @Injectable({
@@ -18,14 +22,14 @@ private urlEndPoint = 'http://localhost:8082/api/clientes';
 
 private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router) { }
 
   getClientes(): Observable<Cliente[]>{
-
+    // return of(CLIENTES)
     return this.http.get(this.urlEndPoint).pipe(
       map( response => response as Cliente[] ) // con map, convertimos el response devuelto a un array de Cliente
     );
-
   }
   
   // Método para crear cliente
@@ -35,7 +39,16 @@ private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
   // Método para recuperar cliente por id
   getCliente(id): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError( e=> { // Con catchError capturamos el error y lo mostramos con SweetAlert2
+        console.error(e.error.mensaje);
+        this.router.navigate(['/clientes']);
+
+        swal('Error al editar', e.error.mensaje, 'error');
+        return throwError(e); // throwError permite convertir la función en un observable, que es el typo de datos que hay que devolver
+
+      })
+    );
   }
 
   // Método para hacer update de cliente
